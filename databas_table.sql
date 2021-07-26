ba5d3c5c262a9d0cf76265da6d55b520bff3a6cf
yaxinxiang <1010234469@qq.com>
2021-07-26 23:32:58 +0800

    Modify database tables design

--- databas_table.sql
+++ databas_table.sql
@@ -1,20 +1,49 @@
 use qiumall;
-drop table user;
+
+##暂时不做改动
+drop table if exists user;
 create table user(
                      id integer auto_increment,
                      username varchar(30) not null unique ,
                      password varchar(30) not null,
                      phone varchar(11) default null unique ,
-                     sex numeric(1,0) check ( sex in (1,2) ),
-                     primary key (id)
+                     status numeric(1,0) default 1, ## 1：买家， 2： 商家， 3： 管理员 以后会用到
+                         sex numeric(1,0) check ( sex in (1,2) ), ## 1为男 2为女
+                         primary key (id)
 );
 
-drop table userimg;
+##暂时不做改动
+drop table if exists userimg;
 create table userimg(
-                        userid integer,
-                        uimgurl varchar(200),
-                        primary key (userid, uimgurl)
+                        id integer auto_increment,
+                        user_id integer not null ,
+                        username varchar(30) not null ,
+                        u_img_url varchar(200)not null ,
+                        primary key (id)
 );
+
+##暂时不做改动
+drop table if exists product;
+create table product(
+                        id integer auto_increment,
+                        name varchar(50) not null ,
+                        price numeric(11,2) default null,
+                        category varchar(30) default null,
+                        primary key (id)
+);
+
+##暂时不做改动
+drop table if exists productimg;
+create table productimg(
+                           id integer auto_increment,
+                           p_id integer,
+                           name varchar(50),
+                           p_img_url varchar(200),
+                           primary key (id)
+);
+
+
+
 
 create table bus(
                     id integer not null ,
@@ -24,23 +53,6 @@ create table bus(
                     foreign key (user_id) references user(id)
 );
 
-drop table product;
-# 测试用具体还需要商议
-create table product(
-                        id integer auto_increment,
-                        name varchar(50) not null ,
-                        price numeric(11,2) default null,
-                        category varchar(30) default null,
-                        primary key (id)
-);
-
-drop table productimg;
-create table productimg(
-                           productid integer,
-                           pimgurl varchar(200),
-                           primary key (productid, pimgurl),
-                           foreign key (productid) references product(id) on delete cascade
-);
 
 create table category(
 
