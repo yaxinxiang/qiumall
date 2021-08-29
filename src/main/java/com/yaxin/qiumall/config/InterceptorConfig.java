6bdf84ca1965c97e7292ec3fc532efbc840bce84
yaxinxiang <1010234469@qq.com>
2021-08-29 20:29:39 +0800

    Accurately intercept requests to achieve the goal of not intercepting static resources

--- src/main/java/com/yaxin/qiumall/config/InterceptorConfig.java
+++ src/main/java/com/yaxin/qiumall/config/InterceptorConfig.java
@@ -17,6 +17,8 @@ public class InterceptorConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(jwtInterceptor())
-                .addPathPatterns("/**");
+                .addPathPatterns("/user/**")
+                .addPathPatterns("/product/**")
+                .addPathPatterns("/upload/**");
     }
 }
